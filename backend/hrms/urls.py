9de3c953cc9112cc4338b74f56d6cc968a8ee5f6
Amir Khan <94292231+imamirullah@users.noreply.github.com>
2026-02-03 20:45:49 +0530

    Update urls.py

--- backend/hrms/urls.py
+++ backend/hrms/urls.py
@@ -1,6 +1,11 @@
+from django.http import JsonResponse
 from django.urls import path, include
 
+def health(request):
+    return JsonResponse({"status": "HRMS Lite API Running"})
+
 urlpatterns = [
+    path("", health),  # 👈 add this
     path("api/employees/", include("employees.urls")),
     path("api/attendance/", include("attendance.urls")),
 ]
